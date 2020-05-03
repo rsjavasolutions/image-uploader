75dda419a028d17059760f52cff8ff8f645ba32c
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-03 21:12:20 +0200

    Created WebSecurityConfig with user in memory

--- src/main/java/com/rsjava/imageuploader/security/WebSecurityConfig.java
+++ src/main/java/com/rsjava/imageuploader/security/WebSecurityConfig.java
@@ -1,21 +1,41 @@
 package com.rsjava.imageuploader.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    //szyfrowanie haseł użytkowników
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        //obiekt odpowiedzialny za szyfrowanie
+        return new BCryptPasswordEncoder();
     }
 
+    //konfiguruję użytkowników
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("jan")
+                .password(passwordEncoder().encode("jan123"))
+                .roles("user");
+    }
+
+    //konfiguruję endpointy
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests()
+                //tylko zalogowani użytkownicy "authenticated()" mają dostęp do endpoint test1
+                .antMatchers("/test1").authenticated()
+                .and()
+                //do formatki logowania będą miały dostęp wszyscy
+                .formLogin();
     }
 }
